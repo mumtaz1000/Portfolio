b7745fad0f1ea1b9c2983fd97e84d14d17dc08b9
mumtaz1000 <shereenfatima1000@gmail.com>
2024-06-11 10:40:52 +0200

    Removing navbar

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open")
-    icon.classList.toggle("open")
-    
+    icon.classList.toggle("open")   
 }
+
+var options = {
+    series: [60, 40],
+    chart: {
+    width: 380,
+    type: 'pie'
+  },
+  labels: ['HTML', 'Team B'],
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      }
+    }
+  }]
+  };
+
+  var chart = new ApexCharts(document.querySelector("#bar-pie-chart"), options);
+  chart.render();
